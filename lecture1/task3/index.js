bd79330ff0cae8017bf4ade2bae913e44ac8c068
Nastia21 <74502108+Nastia21@users.noreply.github.com>
2022-01-26 13:10:34 +0200

    Delete index.js

--- lecture1/task3/index.js
+++
@@ -1,6 +0,0 @@
-export const createPhoneNumber = (number) => {
-    let lenArrNum = number.length;
-    number = number.join("");
-    return "(" + number.slice(0, 3) + ") " + number.slice(3, 6) + '-' + number.slice(6, lenArrNum);
-
-}
