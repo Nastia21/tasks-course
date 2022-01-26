9ef7bb25cd9e16da01779520d3f1669822b0ea6a
Nastia21 <74502108+Nastia21@users.noreply.github.com>
2022-01-26 21:06:08 +0200

    Add files via upload

---
+++ lecture1/task1/index.js
@@ -0,0 +1,6 @@
+
+/*const add = (a,b) => a+b*/
+
+export function add(a,b){
+    return a + b;   
+}
---
+++ lecture1/task1/index.test.js
@@ -0,0 +1,13 @@
+import {add} from './'
+
+describe ('add', ()=>{ 
+test('Should return 4 when passed 2 and 2', 
+function(){
+    const result = add(2,2)
+    expect(result) .toBe(5)
+});
+});
+
+
+
+/*первый аргумент - строка, описание*/
