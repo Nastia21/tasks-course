864cc1060ad07391d0cbc23a7e9c816092d7b5a9
Nastia21 <74502108+Nastia21@users.noreply.github.com>
2022-01-26 13:12:10 +0200

    Add files via upload

---
+++ lecture1/task4/index.test.js
@@ -0,0 +1,35 @@
+import { getOperands } from './'
+
+describe('getOperands', () => {
+    test('Should return a: 1 b: 2',
+        function () {
+            const result = getOperands("1 + 2")
+            expect(result).toBe('a: 1 b: 2');
+        });
+
+    test('Should return a: 1.2 b: -3.4',
+        function () {
+            const result = getOperands("1.2 * -3.4")
+            expect(result).toBe('a: 1.2 b: -3.4');
+        });
+
+    test('Should return a: 1 b: 2',
+        function () {
+            const result = getOperands("1 - 2")
+            expect(result).toBe('a: 1 b: 2');
+        });
+
+    test('Should return a: 1 b: -2',
+        function () {
+            const result = getOperands("1 - -2")
+            expect(result).toBe('a: 1 b: -2');
+        });
+
+    test('Should return a: 15 b: 5',
+        function () {
+            const result = getOperands("15 / 5")
+            expect(result).toBe('a: 15 b: 5');
+        });
+});
+
+
