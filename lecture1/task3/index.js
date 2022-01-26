95634ab393b36e103214d04e39ec1d70a3fd2d78
Nastia21 <74502108+Nastia21@users.noreply.github.com>
2022-01-26 21:08:11 +0200

    Update index.js

--- lecture1/task3/index.js
+++ lecture1/task3/index.js
@@ -4,7 +4,7 @@ export const createPhoneNumber = (number) => {
     }
 
     number = number.join("");
-    return "(" + number.slice(0, 3) + ") " + number.slice(3, 6) + '-' + number.slice(6, );
+    return "(" + number.slice(0, 3) + ") " + number.slice(3, 6) + '-' + number.slice(6);
 
 
 }
