6264cf4ffd645c3cecd258e8ba16e141c3338427
Nastia_Zhyrnova <74502108+Nastia21@users.noreply.github.com>
2022-01-30 15:32:33 +0200

    Update index.js

--- lecture1/task6/index.js
+++ lecture1/task6/index.js
@@ -1,10 +1,9 @@
 export const isPalindrome = (strTask) => {
-    strTask = strTask.replace(/[, . ! ? \s ]/g, "");
+    strTask = strTask.replace(/[,.!?\s]/g, "");
     const strLowerCase = strTask.toLowerCase();
 
     const strArr = strLowerCase.split('');
     const strReversed = strArr.reverse().join('');
 
-    const result = (strReversed == strLowerCase) ? true : false;
-    return result;
+    return strReversed === strLowerCase;
 }
