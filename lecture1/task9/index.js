ffb8ae78425f4ff575fdaf931bef6435e8d3de21
Nastia_Zhyrnova <74502108+Nastia21@users.noreply.github.com>
2022-01-30 16:08:04 +0200

    Update index.js

--- lecture1/task9/index.js
+++ lecture1/task9/index.js
@@ -1,8 +1,8 @@
 export const vowelCount = (strTask) => {
     const strArr = strTask.match(/[aeiou]/g);
-    if (strArr == null){
+    if (strArr === null){
         return 0;
     }
-    const count = strArr.length;
-    return count;
+    
+    return strArr.length;
 }
