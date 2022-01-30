e1456514da41fdb116dc85e3ac39fa25e7dc1feb
Nastia_Zhyrnova <74502108+Nastia21@users.noreply.github.com>
2022-01-30 16:13:07 +0200

    Update index.js

--- lecture1/task4/index.js
+++ lecture1/task4/index.js
@@ -1,6 +1,6 @@
 export const getOperands = (strTask) => {
     const separator = ' ';
-    let strNewArr = strTask.split(separator);
+    const strNewArr = strTask.split(separator);
     
     return 'a: ' + strNewArr[0] + ", b: " + strNewArr[2];
 }
