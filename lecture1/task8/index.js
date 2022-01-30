848ee691eeaa63ea1c5bbb700ecfb164b3ad8235
Nastia_Zhyrnova <74502108+Nastia21@users.noreply.github.com>
2022-01-30 15:35:16 +0200

    Update index.js

--- lecture1/task8/index.js
+++ lecture1/task8/index.js
@@ -2,11 +2,10 @@ export const countOccurrences = (strTask, letter) => {
 
     const regex = new RegExp("[" + letter + "]", "gi");
     const output = strTask.match(regex);
-    if (output == null)
+      if (output === null)
     {
         return 0;
     }
-    const count = output.length;
   
-    return count;
+    return output.length;
 }
