47d2283c00289a7e72a5dc94b1c49b7371d7df45
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-01 11:56:11 +0200

    Update reverseWords.test.js

--- lecture2/task5/reverseWords.test.js
+++ lecture2/task5/reverseWords.test.js
@@ -1,14 +1,14 @@
 import { reverseWords } from './';
 
 describe('reverseWords', () => {
-  test('Should return true, num = 2',
+  test('Should return elbuod  secaps ',
     function() {
       const result = reverseWords('double  spaces');
 
       expect(result).toBe('elbuod  secaps');
     });
 
-  test('Should return true, num = 2',
+  test('Should return ehT kciuq nworb xof spmuj revo eht yzal .god',
     function() {
       const result = reverseWords('The quick brown fox jumps over the lazy dog.');
 
