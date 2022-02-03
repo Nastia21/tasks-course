2555a2e21c522a1c04c4c65245fb3e7befb93af9
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-03 16:59:02 +0200

    added constants, changed result = [ ]

--- lecture2/task2/openOrSenior.js
+++ lecture2/task2/openOrSenior.js
@@ -1,16 +1,18 @@
 export const openOrSenior = (infoWorker) => {
 
+  const MINAGE = 40;
+  const MINEXPERIENCE = 15;
   const strOfInfo = infoWorker.join('|');
   const pattern = /(\d{2},\d+)/g;
   const arrOfDataOfWorkers = strOfInfo.match(pattern);
   
-  const result = new Array();
+  const result = [];
   
   for(let i = 0; i < arrOfDataOfWorkers.length; i++){
 
     const strOfAgeAndExperience = arrOfDataOfWorkers[i].split(',');
 
-    if(strOfAgeAndExperience[0] >= 40 && strOfAgeAndExperience[1] >= 15){
+    if(strOfAgeAndExperience[0] >= MINAGE && strOfAgeAndExperience[1] >= MINEXPERIENCE){
       result.push('Senior'); 
     } else{
       result.push('Open');
