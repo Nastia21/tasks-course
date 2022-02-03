8ba4ed002e7f19c9e02dd74f66c304a2f2a3fa6b
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-03 15:10:59 +0200

    Update invertColor.js

--- lecture2/task7/invertColor.js
+++ lecture2/task7/invertColor.js
@@ -16,7 +16,7 @@ export const invertColor = (inputHex) => {
     arrInvertHex.push(valueHex[resultIndex].toLowerCase());
   }
 
-  return('#' + arrInvertHex.join(''));
+    return `#${arrInvertHex.join('')}`;
  
 };
 
