16ea1ed20e25fc1bbc6fdf8c89d52d3b38acbecb
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-03 17:04:27 +0200

    added checking of incoming number

--- lecture2/task3/isPrime.js
+++ lecture2/task3/isPrime.js
@@ -1,16 +1,24 @@
 export const isPrime = (inputNum) => {
-  const flag = true;
-
-  if(inputNum <= 1){
-    return !flag;
-  }
   
-  for (let i = 2; i < inputNum; i++) {
-    if (inputNum % i === 0) {
-      return !flag; 
-    } 
+  if(!Number.isInteger(inputNum)){
+    return 'Error with input number';
+  } else{
+    
+    const flag = true;
+    
+    if(inputNum <= 1){
+      return !flag;
+    }
+    
+    for (let i = 2; i < inputNum; i++) {
+      if (inputNum % i === 0) {
+        return !flag; 
+      } 
+    }
+    
+    return flag;
   }
-
-  return flag;
 };
+
+
 
