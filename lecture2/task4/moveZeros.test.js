05d3778d5a32c5c7b19c7863682f2849bbc5a876
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-03 17:29:53 +0200

    Update moveZeros.test.js

--- lecture2/task4/moveZeros.test.js
+++ lecture2/task4/moveZeros.test.js
@@ -7,4 +7,11 @@ describe('moveZeros', () => {
 
       expect(result).toEqual([false,1,1,2,1,3,'a',0,0]);
     });
+
+  test('Should return arr = [1, 2, 3, 1, 0, 0 ]',
+    function() {
+      const result = moveZeros([1, 2, 0, 0, 3, 1]);
+      
+      expect(result).toEqual([1, 2, 3, 1, 0, 0 ]);
+    });
 });
