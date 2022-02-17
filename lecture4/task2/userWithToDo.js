b8720277c76cfb28f1fc5b97307e7cf2deed93af
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-17 15:43:37 +0200

    Added userWithToDo().catch

--- lecture4/task2/userWithToDo.js
+++ lecture4/task2/userWithToDo.js
@@ -1,23 +1,27 @@
 export const userWithToDo = async() => {
-  try{
-    const usersUrl = 'https://jsonplaceholder.typicode.com/users';
-    const todoUrl = 'https://jsonplaceholder.typicode.com/todos';
+  const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  const todoUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  const responseUser = await fetch(usersUrl);
+
+  const responseToDo = await fetch(todoUrl);
+
+  const dataUsers = await responseUser.json();
   
-    const responseUser = await fetch(usersUrl);
+  const dataDo = await responseToDo.json();
+  
+  const filteredComplete = dataDo.filter(checkDo => checkDo.completed);
+  
+  const objTotal = dataUsers.map(obj=>Object.assign({}, obj, 
+    {tasks: filteredComplete.filter(checkDo => (checkDo.completed &&  (obj.id ==  checkDo.userId)))}));
 
-    const responseToDo = await fetch(todoUrl);
-
-    const dataUsers = await responseUser.json();
-
-    const dataDo = await responseToDo.json();
-
-    const filteredComplete = dataDo.filter(checkDo => checkDo.completed);
-
-    const objTotal = dataUsers.map(obj=>Object.assign({}, obj, {
-      toDoComplete: filteredComplete.filter(checkDo => (checkDo.completed &&  (obj.id ==  checkDo.userId)))}));
-      
-    return objTotal;
-  } catch (error){
-    throw new Error('Error with data');
-  }
+  return objTotal;
+  
 };
+
+userWithToDo().catch(error => {
+  
+  return error.message; 
+
+});
