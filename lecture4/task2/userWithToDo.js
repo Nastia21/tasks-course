7f6e38a7bd7e18908188535714340ab39f47dc49
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-17 15:33:32 +0200

    Added 1,2 tasks

---
+++ lecture4/task2/userWithToDo.js
@@ -0,0 +1,23 @@
+export const userWithToDo = async() => {
+  try{
+    const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+    const todoUrl = 'https://jsonplaceholder.typicode.com/todos';
+  
+    const responseUser = await fetch(usersUrl);
+
+    const responseToDo = await fetch(todoUrl);
+
+    const dataUsers = await responseUser.json();
+
+    const dataDo = await responseToDo.json();
+
+    const filteredComplete = dataDo.filter(checkDo => checkDo.completed);
+
+    const objTotal = dataUsers.map(obj=>Object.assign({}, obj, {
+      toDoComplete: filteredComplete.filter(checkDo => (checkDo.completed &&  (obj.id ==  checkDo.userId)))}));
+      
+    return objTotal;
+  } catch (error){
+    return new Error;
+  }
+};
