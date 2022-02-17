826f91221b28022bcb2c316abfeeb7838ca5c27b
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-17 15:48:04 +0200

    Changed names of variables

--- lecture4/task1/sortedProductList.js
+++ lecture4/task1/sortedProductList.js
@@ -2,13 +2,12 @@ export const sortedProductList = async() => {
  
   const url = 'https://fakestoreapi.com/products';
 
-  const responce = await fetch(url);
+  const response = await fetch(url);
   
-  const data = await responce.json();
+  const data = await response.json();
   
-  data.sort((a, b) => a.category.localeCompare(b.category) );
-  data.sort((a, b) => (a.price < b.price) ? 1 : -1);
+  data.sort((firstObj, secondObj) => (firstObj.price < secondObj.price) ? 1 : -1);
+  data.sort((firstObj, secondObj) => firstObj.category.localeCompare(secondObj.category));
 
   return data;
-  
 };
