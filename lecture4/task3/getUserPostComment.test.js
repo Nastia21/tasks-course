671e9b64fa345844a822a50ec81ff4293a46bb85
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-02-22 13:03:40 +0200

    Added 2 tests

--- lecture4/task3/getUserPostComment.test.js
+++ lecture4/task3/getUserPostComment.test.js
@@ -9,40 +9,41 @@ beforeEach(() => {
 });
 
 describe('getUserPostComment', () => {
-
   const users = [{
     'id': 1,
     'name': 'Leanne Graham',
     'username': 'Bret'
   },];
-  
+
   const posts = [{
     'userId': 1,
     'id': 1,
     'title': 'sunt aut facere repellat',
   }];
-  
+
   const comments = [{
     'postId': 1,
     'id': 1,
     'body': 'laudantium enim quasi est',
   },];
 
-test('axios will be called 3 times',
+  test('axios will be called 3 times',
     async () => {
-
       axios.get.mockResolvedValueOnce(() => Promise.resolve({ data: users }));
       axios.get.mockResolvedValueOnce(() => Promise.resolve({ data: posts }));
       axios.get.mockResolvedValueOnce(() => Promise.resolve({ data: comments }));
 
+      await getUserPostComment();
+
       expect(axios.get).toHaveBeenCalledTimes(3);
     });
-    test("should return empty users list", async () => {
-      axios.get.mockRejectedValueOnce(null);
 
-      const result = await getUserPostComment();
+  test("should return empty users list", async () => {
+    axios.get.mockRejectedValueOnce(null);
 
-      expect(result).toEqual([]);
-    });
+    const result = await getUserPostComment();
+
+    expect(result).toEqual([]);
+  });
 
 });
